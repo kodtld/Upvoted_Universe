a3862f49d2f6f1e93707880424e1fb60765e17cd
kodtld <kodu@live.fi>
2023-05-15 14:47:39 +0300

    Working program

--- src/content_creator/backgroundService.py
+++ src/content_creator/backgroundService.py
@@ -18,27 +18,16 @@ class VideoSectionExtractor:
 
     def extract_section(self):
         clip = VideoFileClip(self.video_path)
-
-        # Get duration of video
         duration = clip.duration
-
-        # Calculate start and end times for desired section
         section_start = random.uniform(0, duration - self.section_length)
         section_end = section_start + self.section_length
-
-        # Extract section
         section = clip.subclip(section_start, section_end)
 
-        # Crop section from center
         w, h = section.size
         x1 = (w - h) // 2
         y1 = 0
         x2 = x1 + h
         y2 = h
-
         section = section.crop(x1=x1, y1=y1, x2=x2, y2=y2)
-
-        # Resize section to desired dimensions
         section = section.resize((1080, 1920))
-
         return section
